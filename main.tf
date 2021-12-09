005d77307d36f36e684f6596e1a9a2d5c299bde4
chris <chris.aguila14@gmail.com>
2021-12-09 14:28:10 -0500

    single web server

--- main.tf
+++ main.tf
@@ -5,10 +5,27 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami		= "ami-0279c3b3186e54acd"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+	      #!/bin/bash
+	      echo "Hello, World" > index.html
+	      nohup busybox httpd -f -p 8080 &
+	      EOF
 
   tags = {
     Name = "terraform-example"
   }
 }
 
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  
+  ingress {
+  from_port = 8080
+  to_port = 8080
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+ }
+}
 
